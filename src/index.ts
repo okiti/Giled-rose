79a3c7b09aba95e1f4216e7f7289067e9d050c31
okiti <osivwiokiti@gmail.com>
2022-07-05 07:58:15 +0100

    feat: add function to collect user input and validate input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('default');
+import prompt from 'prompt-sync';
+
+const input = prompt();
+
+const collectInput = (question: string): number => {
+  let error: boolean = false;
+  let userInput: string;
+  do {
+    error = false;
+    userInput = input(`${question}: `);
+    if (!parseInt(userInput, 10)) {
+      console.log(`Value must be number, recieved a ${typeof userInput}`);
+      error = true;
+    }
+  } while (error);
+  return parseInt(userInput, 10);
+}
+
+const main = (): void => {
+  const noOfLoops = collectInput('How many times should the shop update?');
+  const noOfStartRequest = collectInput('Number of start requests to the mock API?');
+
+  console.log(noOfLoops, noOfStartRequest);
+};
+main();
